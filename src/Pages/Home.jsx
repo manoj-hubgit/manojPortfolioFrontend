83c0a4ee886a6766196873878d130836fdbabf9d
manoj-hubgit <manojannadurai2265@gmail.com>
2024-12-18 07:56:09 +0530

    Resume updated

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -49,7 +49,7 @@ const Home = () => {
             
               <a
                 className="resume-btn"
-                href="https://drive.google.com/file/d/1Vb72hVIb6otDa7MxfOYOtbkDzIE0fSHB/view?usp=sharing"
+                href="https://drive.google.com/file/d/1g7V-Hmlw64OU5nT0KdKIpT3Vp1hqxcnM/view?usp=sharing"
                 target="_blank"
               ><button className="res-btn btn mx-2 w-25 mb-2">
                 Resume</button>
